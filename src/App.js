99d8078789b17d1e18633321cb13228cbf8686cb
Janique-ka John <spjaniqueka@gmail.com>
2020-10-01 23:14:08 +0200

    code cleanup

--- src/App.js
+++ src/App.js
@@ -3,31 +3,25 @@ import { Navbar } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ConstituencyCard from "./ConstituencyCard";
-import data from './data.js'
+import data from "./data.js";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Navbar bg="light" variant="light">
-        <Navbar.Brand href="#home">SVG VOTES</Navbar.Brand>
-      </Navbar>
-    </div>
-    
-    
-  {
-  data.map((obj) => {
+      <div className="App">
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">SVG VOTES</Navbar.Brand>
+        </Navbar>
+      </div>
 
-        return <ConstituencyCard constituency={obj.constituency} candidates={obj.candidates}/>
-  })
-}
-       
-
-    
-    
-      
-      
-   
+      {data.map((obj) => {
+        return (
+          <ConstituencyCard
+            constituency={obj.constituency}
+            candidates={obj.candidates}
+          />
+        );
+      })}
     </>
   );
 }
