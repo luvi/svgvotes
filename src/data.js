5ca4f5c45d6421b60bc5b43bb1e2718fd9f4d162
Janique-ka John <spjaniqueka@gmail.com>
2020-10-02 00:44:37 +0200

    add more data

--- src/data.js
+++ src/data.js
@@ -24,5 +24,31 @@ export default [
           "https://www.ndpsvg.org/candidates/honourable-terrance-n-ollivierre",
       },
     ]
+  },
+  {
+    constituency: "Northern Grenadines",
+    candidates: [
+      {
+        name: "Carlos Williams",
+        photo: "images/Carlos Williams.jpg",
+        party: "ULP",
+        occupation: "-",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore: 0,
+        website: "",
+      },
+      {
+        name: "Godwin Friday",
+        photo: "images/Hon Dr. Godwin L. Friday-Nothern Granadines.jpg",
+        party: "NDP",
+        occupation: "Leader of the Opposition/ MP Northern Grenadines/Lawyer",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website:
+          "https://www.ndpsvg.org/candidates/honourable-dr-godwin-l-friday",
+      },
+    ]
   }
 ];
