d99e915c27c2070df2ae034f5c72d614821974f7
Janique-ka John <spjaniqueka@gmail.com>
2020-10-02 16:59:21 +0200

    update data

--- src/data.js
+++ src/data.js
@@ -50,5 +50,31 @@ export default [
           "https://www.ndpsvg.org/candidates/honourable-dr-godwin-l-friday",
       },
     ]
+  },
+  {
+    constituency: "Central Kingstown",
+    candidates: [
+      {
+        name: "Dominic Sutherland",
+        photo: "images/Dominic Sutherland.jpg",
+        party: "ULP",
+        occupation: "-",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore: 0,
+        website: "",
+      },
+      {
+        name: "St. Clair Leacock",
+        photo: "images/Hon St.Clair Lecock Central Kingstown.jpg",
+        party: "NDP",
+        occupation: "MP Central Kingstown",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website:
+          "https://www.ndpsvg.org/candidates/honourable-dr-godwin-l-friday",
+      },
+    ]
   }
 ];
