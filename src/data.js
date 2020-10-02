31e1617ceaed40ad6cb161cda90e4b67beec44ab
Janique-ka John <spjaniqueka@gmail.com>
2020-10-02 20:21:27 +0200

    last set of contituencies

--- src/data.js
+++ src/data.js
@@ -216,5 +216,186 @@ export default [
           "",
       }
     ]
+  },
+  {
+    constituency: "North Leeward",
+    candidates: [
+      {
+        name: "Carlos James",
+        photo: "images/Carlos James.jpg",
+        party: "ULP",
+        occupation: "Senator/ Speaker of the House of Assembly/ Lawyer",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore: 0,
+        website: "",
+      },
+      {
+        name: "Roland \"Patel\" Matthews",
+        photo: "images/Hon Roland Patel Mathews North Leeward.jpg",
+        party: "NDP",
+        occupation: "MP North Leeward",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website:
+          "https://www.ndpsvg.org/candidates/honourable-roland-u-matthews",
+      },
+    ]
+  },
+  {
+    constituency: "South Leeward",
+    candidates: [
+      {
+        name: "Mineva Glasgow",
+        photo: "images/Minerva Glasgow.jpg",
+        party: "ULP",
+        occupation: "Retired - Former Deputy Director [NIS]",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore: 0,
+        website: "",
+      },
+      {
+        name: "Nigel \"Nature\" Stephenson ",
+        photo: "images/Hon Nigel Nature Stephen South Leeward.jpg",
+        party: "NDP",
+        occupation: "",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website:
+          "https://www.ndpsvg.org/candidates/honourable-nigel-stephenson",
+      },
+    ]
+  },
+  {
+    constituency: "Central Leeward",
+    candidates: [
+      {
+        name: "Orando Brewster",
+        photo: "images/Minerva Glasgow.jpg",
+        party: "ULP",
+        occupation: "Medical Doctor",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore: 0,
+        website: "",
+      },
+      {
+        name: "Benjamin \"Ben\" Exeter",
+        photo: "images/Benjamin Exeter- Central Leeward.jpg",
+        party: "NDP",
+        occupation: "Entrepreneur",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website:
+          "https://www.ndpsvg.org/candidates/honourable-nigel-stephenson",
+      },
+    ]
+  },
+  {
+    constituency: "North Windward",
+    candidates: [
+      {
+        name: "Montgomery Daniel",
+        photo: "images/montgomery-daniel.jpg",
+        party: "ULP",
+        occupation: "Minister of Housing, Informal Human Settlements, Lands and Surveys and Physical Planning",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website: "",
+      },
+      {
+        name: "Shevern John",
+        photo: "images/Shevern John.jpg",
+        party: "NDP",
+        occupation: "Educator",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore: 0,
+        website:
+          "https://www.ndpsvg.org/candidates/honourable-nigel-stephenson",
+      },
+      {
+        name: "Evrist G Tempro",
+        photo: "images/Evrist G Tempro.jpg",
+        party: "UPP",
+        occupation: "-",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore: 0,
+        website:
+          "http://www.gov.vc/index.php/hon-montgomery-daniel",
+      }
+    ]
+  },
+  {
+    constituency: "South Central Windward",
+    candidates: [
+      {
+        name: "Saboto Caesar",
+        photo: "images/Saboto_Caesar_2015.png",
+        party: "ULP",
+        occupation: "Minister of Agriculture, Forestry, Fisheries, Rural Transformation, Industry & Labour",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website: "http://www.gov.vc/index.php/hon-saboto-caesar",
+      },
+      {
+        name: "Israel Bruce",
+        photo: "images/Israel Bruce.jpg",
+        party: "NDP",
+        occupation: "Opposition Senator/Lawyer",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 0,
+        electedBefore:0,
+        website:
+          "https://www.ndpsvg.org/candidates/honourable-nigel-stephenson",
+      },
+    ]
+  },
+  {
+    constituency: "South Windward",
+    candidates: [
+      {
+        name: "Frederick \"Gustaus\" Stephenson",
+        photo: "images/Minerva Glasgow.jpg",
+        party: "ULP",
+        occupation: "Minister of National Mobilization, Social Development, The Family, Persons with Disabilities and Youth",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website: "http://www.gov.vc/index.php/hon-frederick-stephenson",
+      },
+      {
+        name: "Noel Dickson",
+        photo: "images/Noel Dickson- South Windward.jpg",
+        party: "NDP",
+        occupation: "Retired Banker",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore:0,
+        website:
+          "https://www.ndpsvg.org/candidates/noel-dickson",
+      },
+    ]
+  },{
+    constituency: "North Central Windward",
+    candidates: [
+      {
+        name: "Ralph Gonsalves",
+        photo: "images/Ralph_Gonsalves_2015_03.jpg",
+        party: "ULP",
+        occupation: "Prime Minister & Minister of National Security, Grenadines Affairs and Legal Affairs",
+        bio: "Jelly-o gummi bears wafer. Oat cake cupcake bonbon toffee. Jelly tiramisu gummi bears jelly beans dragée dragée cupcake fruitcake.Jelly beans pastry toffee halvah caramels. Jujubes chocolate cake croissant powder marshmallow lemon drops jujubes gingerbread gingerbread",
+        ranBefore: 1,
+        electedBefore: 1,
+        website: "http://www.gov.vc/index.php/dr-hon-ralph-gonsalves",
+      }
+    ]
   }
 ];
